60ab791868baca9a2c466bc2d42b6fff11a9dd48
Lalith Kumar <lalithkumar19180@gmail.com>
2024-02-03 22:10:29 +0530

    added

--- src/pages/Indexpage.jsx
+++ src/pages/Indexpage.jsx
@@ -3,6 +3,8 @@ import Post from "../Post";
 import { useState } from "react";
 import { UserContext } from "../Usercontext";
 import { useContext } from "react";
+import Loaderpage from './Loaderpage';
+
 
 export default function Indexpage() {
 
@@ -26,19 +28,18 @@ export default function Indexpage() {
     if (userinfo === null || userinfo === '') {
         window.location.reload();
     }
-    console.log("posts are", posts);
 
     return (
         <>
             {
 
-                posts ? (posts.map(per => {
+                Array.isArray(posts)&&posts.length!==0 ? (posts.map(per => {
                     return <Post key={per._id} title={per.title} summary={per.summary} create_at={per.createdAt} _id={per._id} cover={per.cover} author={per.author.username} />
 
 
                 })) : (
                     <>
-                        <Post title="no data is fetched" summary="no data is fetched from db" />
+                    <Loaderpage/>
                     </>
                 )
             }
